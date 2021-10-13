4b3ae56f32e594d59d116619603e844b8a0aec89
AlexMood99 <91706219+AlexMood99@users.noreply.github.com>
2021-10-13 18:54:27 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var movingRect, fixedRect;
+
+
+function setup() {
+  createCanvas(800,400);
+  movingRect = createSprite(400, 200, 80, 30);
+  movingRect.shapeColor = "green";
+  fixedRect = createSprite(200, 200, 50, 80);
+  fixedRect.shapeColor = "green";
+}
+
+function draw() {
+  background(0);  
+  console.log(movingRect.x - fixedRect.x);
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+  if(movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2 && 
+    fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2){
+    movingRect.shapeColor = "red";
+    fixedRect.shapeColor = "red";
+  }
+  else{
+    movingRect.shapeColor = "green";
+    fixedRect.shapeColor = "green"; 
+  }
+  drawSprites();
+}
